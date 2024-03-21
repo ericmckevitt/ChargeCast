555453f37e9b5a99de8251687a58e2c723bddd81
Eric McKevitt <erictmckevitt@gmail.com>
2024-03-20 19:04:59 -0600

    first commit

---
+++ src/test/java/com/example/chargecast/ChargeCastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chargecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeCastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
