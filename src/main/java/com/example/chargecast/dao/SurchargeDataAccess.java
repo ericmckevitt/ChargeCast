c8ab4441ae72f2ac215e00c8a226f62f6611b678
Ryan <mckevitt.r@gmail.com>
2024-03-23 18:17:05 -0500

    linked dao layer to grades repository - allegedly

--- src/main/java/com/example/chargecast/dao/SurchargeDataAccess.java
+++ src/main/java/com/example/chargecast/dao/SurchargeDataAccess.java
@@ -1,4 +1,16 @@
 package com.example.chargecast.dao;
 
+import com.example.chargecast.repositories.GradesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SurchargeDataAccess {
+
+    @Autowired
+    private final GradesRepository gradesRepository;
+
+    public SurchargeDataAccess(GradesRepository gradesRepository) {
+        this.gradesRepository = gradesRepository;
+    }
 }
--- src/main/java/com/example/chargecast/repositories/GradesRepository.java
+++ src/main/java/com/example/chargecast/repositories/GradesRepository.java
@@ -1,4 +1,12 @@
 package com.example.chargecast.repositories;
 
-public interface GradesRepository {
+import com.example.chargecast.models.GradeModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface GradesRepository extends JpaRepository<GradeModel, Integer> {
+//    @Query("SELECT * FROM Grades")
+//    List<GradeModel> findAllGrades();
 }
